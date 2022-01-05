0a624436fbd601e38103fc06c907c60e0ca0b873
Nguyenphangiang <phancruel@gmail.com>
2022-01-05 17:14:48 +0700

    up1

---
+++ myjs.js
@@ -0,0 +1,21 @@
+function cong1(){
+    let number1= Number(document.getElementById('number1').value);
+    let number2= Number(document.getElementById('number2').value);
+    let tong= number1 + number2;
+    document.getElementById('result').innerText='Result Division:'+tong
+}function tru1(){
+    let number1= Number(document.getElementById('number1').value);
+    let number2= Number(document.getElementById('number2').value);
+    let tong= number1 - number2;
+    document.getElementById('result').innerText='Result Division:'+tong
+}function nhan1(){
+    let number1= Number(document.getElementById('number1').value);
+    let number2= Number(document.getElementById('number2').value);
+    let tong= number1 * number2;
+    document.getElementById('result').innerText='Result Division:'+tong
+}function chia1(){
+    let number1= Number(document.getElementById('number1').value);
+    let number2= Number(document.getElementById('number2').value);
+    let tong= number1 / number2;
+    document.getElementById('result').innerText='Result Division:'+tong
+}
